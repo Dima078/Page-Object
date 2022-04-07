20d69eda05b0c636ec227256f73b3d9fe9a829fb
Dmitry Kovtun <mr_write@mail.ru>
2022-04-07 14:48:21 +0300

    begin2

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -9,9 +9,9 @@ import static com.codeborne.selenide.Selenide.open;
 class MoneyTransferTest {
     @Test
     void shouldTransferMoneyBetweenOwnCardsV1() {
-        open("http://localhost:9999");
-        var loginPage = new LoginPageV1();
-//    var loginPage = open("http://localhost:9999", LoginPageV1.class);
+        //open("http://localhost:9999");
+        //var loginPage = new LoginPageV1();
+        var loginPage = open("http://localhost:9999", LoginPageV1.class);
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
